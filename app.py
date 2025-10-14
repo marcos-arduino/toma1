21ce6214a5340d18b214ebea71f2dc0533558528
marcos-arduino <arduino.marcoset36@gmail.com>
2025-10-14 01:08:02 -0300

    creacion de archivos de la db para testear con test.html la api

--- app.py
+++ app.py
@@ -17,15 +17,26 @@ def get_db_connection():
 def home():
     return render_template("index.html")
 
+@app.route("/peliculas")
+def cargar_peliculas():
+    conn = get_db_connection()
+    pelicula = conn.execute("SELECT * FROM peliculas").fetchall()
+    conn.commit()
+    conn.close()
+    return render_temptes("test.html")
 
-@app.route("/peliculas/<int:id>", methods=["GET"])
-def cargar_pelicula(id):
-	conn = get_db_connection()
-	pelicula = conn.execute("SELECT * FROM peliculas WHERE id = ?", (id,)).fetchone()
-	conn.commit()
-	conn.close()
-	return render_template("pelicula.html", pelicula=pelicula)
-    
+
+
+
+# @app.route("/peliculas/<int:id>", methods=["GET"])
+# def cargar_pelicula(id):
+# 	conn = get_db_connection()
+#	pelicula = conn.execute("SELECT * FROM peliculas WHERE id = ?", (id,)).fetchone()
+#	conn.commit()
+#	conn.close()
+#	return render_template("pelicula.html", pelicula=pelicula)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
